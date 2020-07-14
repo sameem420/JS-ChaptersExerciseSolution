222f048a7258a9f3e2fb698877b0aaeeab99127b
Muhammad Sameem <43237227+sameem420@users.noreply.github.com>
2020-07-14 22:41:26 +0500

    Update app.js

--- Chapter # 38-42/app.js
+++ Chapter # 38-42/app.js
@@ -10,12 +10,16 @@
 
 // alert(power(5,3));
 
+
 // Task # 2
-var year = prompt("Enter the year :");
-if (((year % 4 == 0) && (year % 100 != 0)) || (year % 400 == 0))
-{
-    alert("Leap year");
-}
-else {
-    alert("Not Leap year");
-}
+// var year = prompt("Enter the year :");
+// if (((year % 4 == 0) && (year % 100 != 0)) || (year % 400 == 0))
+// {
+//     alert("Leap year");
+// }
+// else {
+//     alert("Not Leap year");
+// }
+
+
+// Task # 3
