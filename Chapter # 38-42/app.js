dbe0cdf9b3f83a37df409db5338db915fcf3263b
Muhammad Sameem <43237227+sameem420@users.noreply.github.com>
2020-07-14 22:39:49 +0500

    Update app.js

--- Chapter # 38-42/app.js
+++ Chapter # 38-42/app.js
@@ -1,11 +1,21 @@
 // Task # 1
-function power(base,power)
-{
-    var p = 1;
-    for (var i=0; i<power; i++) {
-        p *= base;
-    }
-  return p;
-}
+// function power(base,power)
+// {
+//     var p = 1;
+//     for (var i=0; i<power; i++) {
+//         p *= base;
+//     }
+//   return p;
+// }
 
-alert(power(5,3));
+// alert(power(5,3));
+
+// Task # 2
+var year = prompt("Enter the year :");
+if (((year % 4 == 0) && (year % 100 != 0)) || (year%400 == 0))
+{
+    alert("Leap year");
+}
+else {
+    alert("Not Leap year");
+}
