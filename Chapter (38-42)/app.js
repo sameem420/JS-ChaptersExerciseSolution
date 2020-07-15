ea549eca9cca0342bd01b522d53eb892b8150de8
Muhammad Sameem <43237227+sameem420@users.noreply.github.com>
2020-07-15 19:57:05 +0500

    Update app.js

--- Chapter (38-42)/app.js
+++ Chapter (38-42)/app.js
@@ -44,16 +44,30 @@
 
 
 // Task # 4
-let firstSubjectmarks = +prompt('Enter first Subject Marks : ');
-let secondSubjectmarks = +prompt('Enter second Subject Marks : ');
-let thirdSujectmarks = +prompt('Enter third Subject Marks: ');
-
-function Average()
+function mainFunction()
 {
-
+    let firstSubjectmarks = +prompt('Enter first Subject Marks : ');
+    let secondSubjectmarks = +prompt('Enter second Subject Marks : ');
+    let thirdSujectmarks = +prompt('Enter third Subject Marks: ');
+    var avg = Average(firstSubjectmarks, secondSubjectmarks, thirdSujectmarks);
+    alert("The average of the marks is : " + avg);
+    var total = 100;
+    Percentage(total,firstSubjectmarks, secondSubjectmarks, thirdSujectmarks);
+}
+function Average(firstSubjectmarks, secondSubjectmarks, thirdSujectmarks)
+{
+    var avg = (firstSubjectmarks + secondSubjectmarks + thirdSujectmarks)/3;
+    return avg;
 }
 
-function Percentage() 
+function Percentage(total,firstSubjectmarks, secondSubjectmarks, thirdSujectmarks) 
 {
-    
+    var firstSubjectPercent = (firstSubjectmarks/total)*100;
+    var secondSubjectPercent = (secondSubjectmarks/total)*100;
+    var thirdSujectPercent = (thirdSujectmarks/total)*100;
+    alert("The percentage of first Subject is : " + firstSubjectPercent);
+    alert("The percentage of second Subject is : " + secondSubjectPercent);
+    alert("The percentage of third Subject is : " + thirdSujectPercent);
 }
+
+mainFunction();
