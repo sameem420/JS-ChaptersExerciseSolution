4aaa3759f46b46fe26eec98ff8b3aca1055b1c15
Muhammad Sameem <43237227+sameem420@users.noreply.github.com>
2020-07-16 22:33:59 +0500

    update app.js

--- Chapter (43-48)/app.js
+++ Chapter (43-48)/app.js
@@ -8,3 +8,12 @@ function deleteRow(id)
 {
     document.getElementById(id).remove();
 }
+
+function changeImage()
+{
+    document.getElementById("myImage").src = 'images/InfinixNote7-b.jpg';
+}
+
+function restoreImage() {
+    document.getElementById("myImage").src = 'images/SamsungGalaxyA31-b.jpg';
+}
