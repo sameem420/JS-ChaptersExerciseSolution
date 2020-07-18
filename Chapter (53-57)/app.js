c8198c42c8c6ec1c275d39c74357a37a57d12df3
Muhammad Sameem <43237227+sameem420@users.noreply.github.com>
2020-07-18 10:01:59 +0500

    update app.js

--- Chapter (53-57)/app.js
+++ Chapter (53-57)/app.js
@@ -1,2 +1,14 @@
 // Task # 1
 var modal = document.getElementById('modal');
+
+var pictures = [
+    'images/3.jpg',
+    'images/4.png',
+    'images/5.jpg',
+    'images/6.jpg'
+];
+
+
+for (i = 0; i < pictures.length; i++) {
+    modal.innerHTML += "<img src='"+pictures[i]+"'>";
+}
