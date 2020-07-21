de2a0a6c5f69b8c6c6b66226684d7232551e019d
Muhammad Sameem <43237227+sameem420@users.noreply.github.com>
2020-07-21 19:30:01 +0500

    added chapter 1 solutions

---
+++ Chapter - 1/index.js
@@ -0,0 +1,11 @@
+// Assignment # 1
+
+alert("Welcome to my Site!");
+
+alert("Error! Please enter a valid password.");
+
+alert("Welcome to JS Land... \n Happy Coding!");
+
+alert("Welcome to JS Land...");
+
+alert("Happy Coding!");
