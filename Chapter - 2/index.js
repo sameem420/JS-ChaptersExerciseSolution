d1c95a1ac3af7c3e60fc342b4950b31da5821068
Muhammad Sameem <43237227+sameem420@users.noreply.github.com>
2020-07-21 19:30:34 +0500

    added chapter 2 solutions

---
+++ Chapter - 2/index.js
@@ -0,0 +1,27 @@
+var username;
+var myName = "Muhammad Sameem";
+var message;
+message = "Hello World!"
+alert(message);
+
+var age = 21;
+var profession = "Certified Mobile Application Developer";
+
+alert(myName);
+alert(age + " years old");
+alert(profession);
+
+var pyramid = "PIZZA\nPIZZ\nPIZ\nPI\nP";
+alert(pyramid);
+
+var email = "sameemgujjar123@yahoo.com";
+alert("My email address is " + email);
+
+var book = "A smarter way to learn JavaScript";
+alert("I am trying to learn from the Book " + book);
+
+document.write("Yah! I can write HTML content through JavaScript");
+
+var stylishText = "▬▬▬▬▬▬▬▬▬ஜ۩۞۩ஜ▬▬▬▬▬▬▬▬▬";
+alert(stylishText);
+document.write(stylishText);
