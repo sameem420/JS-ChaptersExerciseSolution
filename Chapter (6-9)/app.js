313cc0980f6de659a4696b870e62035f003fd36e
Muhammad Sameem <43237227+sameem420@users.noreply.github.com>
2020-07-21 19:46:10 +0500

    Update app.js

--- Chapter (6-9)/app.js
+++ Chapter (6-9)/app.js
@@ -4,6 +4,7 @@
 // 1. Write a program to take a number in a variable, do the
 // required arithmetic to display the following result in your
 // browser:
+
 var a=10;
 document.write("<br><br>The value of a is: "+a)
 ++a;
@@ -22,15 +23,22 @@ document.write("<br>Now the value of a is: "+a+"<br><br>");
 // question2
 // 2. What will be the output in variables a, b & result after
 // execution of the following script:
+
 var a = 2, b = 1;
 var result = --a - --b + ++b + b--;
+
 // Explain the output at each stage:
+
 --a;
+
 // At --a it is 1 because the value is 1
+
 --a - --b;
+
 // At this stage b is 0 so --a - --b=1;
---a - --b + ++b = 1 - 0 + 0=2;
---a - --b + ++b + b--=;
+// --a - --b + ++b = 1 - 0 + 0=2;
+// --a - --b + ++b + b--=;
+
 var a = 2, b = 1;
 var result = --a - --b + ++b + b--;
 document.write("result is "+result+"<br>");
@@ -38,6 +46,7 @@ document.write("result is "+result+"<br>");
 // question3
 // 3. Write a program that takes input a name from user &
 // greet the user.
+
 var name=prompt("Whats your name?");
 alert("Your name is " + name);
 
@@ -47,6 +56,7 @@ alert("Your name is " + name);
 // display it’s multiplication table on your browser. If user
 // does not enter a new number, multiplication table of 5
 // should be displayed by default.
+
 var num=prompt("Enter a number: ");
 var num1=5;
 var mul;
@@ -72,19 +82,26 @@ else
 // 6. Take
 // a) Take three subjects name from user and store them in 3
 // different variables.
+
 var sub1,sub2,sub3;
 sub1=prompt("Enter first subject name");
 sub2=prompt("Enter second subject name");
 sub3=prompt("Enter third subject name");
+
 // b) Total marks for each subject is 100, store it in another
 // variable.
+
 var total_marks = 100;
+
 // c) Take obtained marks for first subject from user and
 // stored it in different variable.
+
 var x=prompt("Enter obtained marks for subject one ");
 var obtained_marks1 = parseInt(x);
+
 // d) Take obtained marks for remaining 2 subjects from user
 // and store them in variables.
+
 var y=prompt("Enter obtained marks for subject two ");
 var obtained_marks2 = parseInt(y);
 var z=prompt("Enter obtained marks for subject three ");
@@ -92,8 +109,10 @@ var obtained_marks3 = parseInt(z);
 var percent1=(obtained_marks1/total_marks)*100;
 var percent2=(obtained_marks2/total_marks)*100;
 var percent3=(obtained_marks3/total_marks)*100;
+
 // e) Now calculate total marks and percentage and show the
 // result in browser like this.(Hint: user table)
+
 var Total=total_marks*3;
 var TotalPercentage;
 var obtained=obtained_marks1+obtained_marks2+obtained_marks3;
