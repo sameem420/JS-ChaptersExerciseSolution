79ccac6e63cd22033204ab14c4eede4caccb5847
Muhammad Sameem <43237227+sameem420@users.noreply.github.com>
2020-07-21 20:17:47 +0500

    Update app.js

--- Chapter (12-13)/app.js
+++ Chapter (12-13)/app.js
@@ -1,10 +1,11 @@
-Chapter 12-13              IF…ELSE & ELSE IF STATEMENT,TESTING SET OF CONDITIONS 
-question1
-1. Write a program that takes a character (number or string)
-in a variable & checks whether the given input is a
-number, uppercase letter or lower case letter. (Hint: ASCII
-codes:- A=65, Z=90, a=97, z=122).
-question1
+// Chapter 12-13     IF…ELSE & ELSE IF STATEMENT,TESTING SET OF CONDITIONS 
+
+// question1
+// 1. Write a program that takes a character (number or string)
+// in a variable & checks whether the given input is a
+// number, uppercase letter or lower case letter. (Hint: ASCII
+// codes:- A=65, Z=90, a=97, z=122).
+
 var num=prompt("Enter the number or string");
 var num1=parseInt(num);
 if(num.charCodeAt(0)>=65&&num.charCodeAt(0)<=90)
@@ -23,14 +24,13 @@ else
 {
     alert("It is something else");
 }
- 
 
 alert(String.fromCharCode(65,66,67)); // returns 'ABC'
 
 
-question2
-2. Write a JavaScript program that accept two integers and
-display the larger. Also show if the two integers are equal.
+// question2
+// 2. Write a JavaScript program that accept two integers and
+// display the larger. Also show if the two integers are equal.
 var num=prompt("Enter the first number");
 var num1=parseInt(num);
 num=prompt("Enter the second number");
@@ -49,9 +49,9 @@ else if(num1==num2)
 }
 
 
-question3
-3. Write a program that takes input a number from user &
-state whether the number is positive, negative or zero.
+// question3
+// 3. Write a program that takes input a number from user &
+// state whether the number is positive, negative or zero.
 var num=prompt("Enter the number");
 var num1=parseInt(num);
 if(num1===0)
@@ -68,9 +68,9 @@ else if(num1<0)
 }
 
 
-question4
-4. Write a program that takes a character (i.e. string of
-length 1) and returns true if it is a vowel, false otherwise
+// question4
+// 4. Write a program that takes a character (i.e. string of
+// length 1) and returns true if it is a vowel, false otherwise
 var len=prompt("Enter a character i.e(The length of string 1)");
 var str=len.length;
 str=parseInt(str);
@@ -90,17 +90,17 @@ else{
 }
 
 
-question5
-5. Write a program that
-a. Store correct password in a JS variable.
-b. Asks user to enter his/her password
-c. Validate the two passwords:
-i. Check if user has entered password. If not, then
-give message “ Please enter your password”
-ii. Check if both passwords are same. If they are
-same, show message “Correct! The password you
-entered matches the original password”. Show
-“Incorrect password” otherwise.
+// question5
+// 5. Write a program that
+// a. Store correct password in a JS variable.
+// b. Asks user to enter his/her password
+// c. Validate the two passwords:
+// i. Check if user has entered password. If not, then
+// give message “ Please enter your password”
+// ii. Check if both passwords are same. If they are
+// same, show message “Correct! The password you
+// entered matches the original password”. Show
+// “Incorrect password” otherwise.
 
 var pas1=prompt("Please! Enter a password");
 var len=pas1.length;
@@ -128,8 +128,8 @@ else if(pas1!=pas2)
 
 
 
-question6
-6. This if/else statement does not work. Try to fix it:
+// question6
+// 6. This if/else statement does not work. Try to fix it:
 var greeting;
 var hour = 13;
 if (hour < 18) {
@@ -141,14 +141,14 @@ else
 greeting = "Good evening";
 alert(greeting);
 }
-The brackets {} were not there correctly
-Answer
+// The brackets {} were not there correctly
+// Answer
 
 
-question7
-7. Write a program that takes time as input from user in 24
-hours clock format like: 1900 = 7pm. Implement the
-following case using if, else & else if statements
+// question7
+// 7. Write a program that takes time as input from user in 24
+// hours clock format like: 1900 = 7pm. Implement the
+// following case using if, else & else if statements
 var time=prompt("Enter the time in 24 hours");
 var timecheck=time.length;
 while(timecheck!=4)
