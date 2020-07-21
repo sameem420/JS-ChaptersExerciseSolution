ff854d592efff1682aac28ca8f5dcc1f24e931d3
Muhammad Sameem <43237227+sameem420@users.noreply.github.com>
2020-07-21 19:32:26 +0500

    added chapters 3,4,5

---
+++ Chapter (3)/index.js
@@ -0,0 +1,15 @@
+var age = 21;
+alert("I am " + age + " years old.");
+
+var visits = 1;
+Number(visits) + 1;
+alert("You have visited this site " + visits + " times")
+
+var birthYear= 1999;
+document.writeln("My birth year is " + birthYear + "<br>");
+document.writeln("Data type of my decalred variable is " + typeof(birthYear) + "<br>");
+
+var visitorName = "John Doe";
+var productTitle = "T-shirt(s)";
+var quantity = 5;
+document.write("<b>" + visitorName + "</b>" + " Ordered " + "<b>" + quantity + " " + productTitle + "</b>" + " On XYZ Clothing Store");
---
+++ Chapter (4)/index.js
@@ -0,0 +1,10 @@
+var name1, name2, name3;
+
+var _name,name,Name,name1,$name;
+//var 1name, %name, function, .name,
+
+document.write("<h2>Rules for naming JS variables</h2><br>");
+document.write("Variable names can only contain ,numbres,$ and _ . For example $my_1stVariable<br>");
+document.write("Variables must begin with a letter,$ or _ . For example $name, _name or name<br>");
+document.write("Variable names are case sensitive<br>");
+document.write("Variable names should not be JS Keywords<br>");
