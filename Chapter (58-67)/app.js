8196b1e018bef2a408b4465d64c0f85478fe4596
Muhammad Sameem <43237227+sameem420@users.noreply.github.com>
2020-07-23 10:02:46 +0500

    Update app.js

--- Chapter (58-67)/app.js
+++ Chapter (58-67)/app.js
@@ -1,2 +1,27 @@
 // Task # 1
 var mainContent = document.getElementById("main-content");
+console.log(mainContent.childNodes);
+
+
+var render = document.getElementsByClassName("render");
+
+for (var i=0; i< render.length; i++)
+{
+    document.writeln(render[i].innerHTML);
+}
+
+
+var firstName = document.getElementById("first-name");
+firstName.value = "Muhammad";
+
+var lastName = document.getElementById("last-name");
+lastName.value = "Sameem";
+
+var email = document.getElementById("email");
+email.value = "sameem@user.com";
+
+
+var formContent = document.getElementById("form-content");
+document.write("<br><br>" + formContent.nodeType);
+
+document.write("<br><br>" + lastName.nodeType);
